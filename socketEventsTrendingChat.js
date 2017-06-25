fb904e1170bcece04268e96bc6e7a264a3ca666e
Avadhesh Bhatt <avadheshbhatt92@gmail.com>
2017-06-25 20:29:39 +0530

    room sokcet

--- socketEventsTrendingChat.js
+++ socketEventsTrendingChat.js
@@ -3,16 +3,16 @@ module.exports = function (ioTrendingChat) {
     ioTrendingChat.on('connection', function (socket) {
         console.log('ioTrendingChat :: someone connected');
 
-        socket.on('join room', function (room) {
-            console.log('** ** ** ioTrendingChat user connected to room : ' + room);
-            socket.join(room);
+        socket.on('join channel', function (channel_name) {
+            console.log('** ** ** ioTrendingChat user connected to room : ' + channel_name);
+            socket.join(channel_name);
         });
 
         socket.on('send message', function (jsonData) {
-            var room_name = jsonData.room_name;
+            var channel_name = jsonData.channel_name;
             var message = jsonData.message;
-            console.log('send message to channel : ' + room_name);
-            ioTrendingChat.to(room_name).emit('get message', jsonData);
+            console.log('send message to channel : ' + channel_name);
+            ioTrendingChat.to(channel_name).emit('get message', message);
         });
     });
 };
