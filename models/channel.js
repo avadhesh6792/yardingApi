acfe7a5b3ce5c68571983508ea4a07f14fd1030e
Avadhesh Bhatt <avadheshbhatt92@gmail.com>
2017-06-30 23:15:29 +0530

    dhc

--- models/channel.js
+++ models/channel.js
@@ -10,7 +10,7 @@ var Channel = new Schema({
     user_id                 : ObjectId,
     members_id              : { type: Array, default: []},
     link                    : { type: String, default: '' }
-}, , {timestamps: true} );
+},  {timestamps: true} );
 
 module.exports = mongoose.model('Channel', Channel);
 
