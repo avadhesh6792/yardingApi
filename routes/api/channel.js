7e6162cec69ed56dc3347f064b10ae21cf2d1e36
Avadhesh Bhatt <avadheshbhatt92@gmail.com>
2017-08-15 22:38:34 +0530

    dfk

--- routes/api/channel.js
+++ routes/api/channel.js
@@ -1102,7 +1102,7 @@ router.get('/testing', function (req, res, next) {
         },
         {
             $group: {
-                _id: '_id',
+                _id: '$_id',
                 members_info : { $push: "$members_info1.0" }
             }
         }
